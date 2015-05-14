d455fc60e0006ea657e5ee5c964b5e8d1c0272bb
Joshua Garcia <j.garcia12998@gmail.com>
2015-05-14 08:47:44 -0700

    Video 9

--- index.php
+++ index.php
@@ -10,6 +10,22 @@ define('clientSecret', 'b73c33a93dea45b3ab1342bcdaab423c');
 define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
 
+//Function that is going to connect to Instagram
+function connectToInstagram($url){
+	$ch = curl_init();
+
+	curl_setopt_array($ch, array(
+		CURLOPT_URL => $url;
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_SSL_VERIFYHOST => 2,
+		));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
+
+
 if(isset($_GET['code'])){
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
